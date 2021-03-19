4ef7ec46bcf925451644e4ef13b390e240f363a5
anjaliesidhilall@gmail.com <anjaliesidhilall@gmail.com>
2021-03-19 10:55:02 -0400

    WEEK 6

---
+++ ChallengesWithTestsMark8/ChallengesSet06Base.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet06Base
+    {
+
+        public double[] GetEveryNthElement(List<double> elements, int n)
+        {
+            List<double> nthElement = new List<double>();
+            double[] nullCheck = new double[0];
+
+            if (elements == null)
+            {
+                return nullCheck;
+            }
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] % n == 0)
+                {
+                    nthElement.Add(elements[i]);
+                }
+
+                if (n < 0 || n > elements.Count)
+                {
+                    nthElement.Clear();
+                }
+            }
+
+            return nthElement.ToArray();
+
+
+        }
+    }
+}
